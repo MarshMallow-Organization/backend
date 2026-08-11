f985de89d278fbc33cbbdc231a759f72fc55c1d0
kwonminsooo <kms37480@naver.com>
2026-08-11 14:50:34 +0900

    fix/configuration.ts

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -20,7 +20,6 @@ export default () => ({
     accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN ?? '15m',
     refreshSecret: process.env.JWT_REFRESH_SECRET,
     refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN ?? '14d',
-    devAccessExpiresIn: process.env.JWT_DEV_ACCESS_EXPIRES_IN ?? '7d',
   },
   google: {
     clientId: process.env.GOOGLE_CLIENT_ID,
