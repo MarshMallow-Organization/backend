1348a4494e0061f8b8ead510e8dd2632945a970c
8t-truck <kwoncar3@gmail.com>
2026-08-15 20:58:28 +0900

    feat:swagger-cli-plugin 반영

--- src/domains/diaries/dto/response/get-diaries-response.dto.ts
+++ src/domains/diaries/dto/response/get-diaries-response.dto.ts
@@ -1,13 +1,19 @@
 import { DiaryPreviewDto } from './diary-preview.dto';
 
 export class GetDiariesResponseDto {
+  /** 조회된 일기 목록. */
   items: DiaryPreviewDto[];
 
+  /** 현재 페이지 번호. @example 0 */
   page: number;
+  /** 페이지 크기. @example 10 */
   size: number;
 
+  /** 필터 조건에 해당하는 전체 일기 수. @example 32 */
   totalElements: number;
+  /** 전체 페이지 수. @example 4 */
   totalPages: number;
 
+  /** 다음 페이지 존재 여부. @example true */
   hasNext: boolean;
 }
